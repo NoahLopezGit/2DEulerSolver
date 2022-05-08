a86dfcb07d987656858381862654b34f0d5a2a50
Noah <loalno12@gmail.com>
2022-05-08 14:09:09 -0700

    changed variable name

--- Simple_FVM/grid_storage.f95
+++ Simple_FVM/grid_storage.f95
@@ -15,16 +15,15 @@ contains
     print *, "Enter Grid Filename "
     read *, filename
     open(1,file=filename,status='old')
-    read(1,*) output
-    print *, output
-    !read(1,*) dimi,dimj
-    !allocate(grid(dimi,dimj,2))
 
-    !do i=1,dimi
-    !   do j=1,dimj
-    !      read(1,*) grid(i,j,1), grid(i,j,2)
-    !   end do
-    !end do
+    read(1,*) width,height
+    allocate(grid(width,height,2))
+
+    do i=1,width
+       do j=1,height
+          read(1,*) grid(i,j,1), grid(i,j,2)
+       end do
+    end do
 
   end subroutine readnstore
 end module grid_storage
